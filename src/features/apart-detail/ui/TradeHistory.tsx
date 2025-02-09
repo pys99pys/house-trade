a3a460ff26c8dc323542a1950135405ec706f81a
박윤서 <ys@bag-yunseoui-MacBookPro.local>
2025-02-10 02:34:47 +0900

    전체 거래 목록 CSS 네스팅 적용

--- src/features/apart-detail/ui/TradeHistory.tsx
+++ src/features/apart-detail/ui/TradeHistory.tsx
@@ -20,33 +20,33 @@ const TradeHistory: FC<TradeHistoryProps> = ({ tradeItems: originTradeItems }) =
     <Box title="전체 거래 목록">
       <div className={css.tradeHistory}>
         <div className={classNames(css.header, css.row, "box")}>
-          <span>
+          <span className={css.cell}>
             <strong>거래일</strong>
           </span>
-          <span>
+          <span className={css.cell}>
             <strong>평수</strong>
           </span>
-          <span>
+          <span className={css.cell}>
             <strong>층</strong>
           </span>
-          <span>
+          <span className={css.cell}>
             <strong>거래금액</strong>
           </span>
         </div>
         <div className={css.body}>
           {tradeItems.map((item) => (
             <div key={item.year} className={css.wrap}>
-              <h2>
+              <h2 className={css.year}>
                 {item.year}년({item.items.length})
               </h2>
               {item.items.map((_item, i) => (
                 <BaseBox key={i} className={css.row}>
-                  <span>{_item.tradeDate}</span>
-                  <span>
+                  <span className={css.cell}>{_item.tradeDate}</span>
+                  <span className={css.cell}>
                     {calculateFlatSize(_item.size)}평<small>({calculateAreaSize(_item.size)}㎡)</small>
                   </span>
-                  <span>{_item.floor}층</span>
-                  <span>
+                  <span className={css.cell}>{_item.floor}층</span>
+                  <span className={css.cell}>
                     <strong>{calculateTradeAmountFormat(_item.tradeAmount)}</strong>
                   </span>
                 </BaseBox>

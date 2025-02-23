31731c47ecd504c936151f555995e3498d691de2
박윤서 <ys@bag-yunseoui-MacBookPro.local>
2025-02-24 01:28:29 +0900

    cors 해제

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,27 @@ const nextConfig = {
       },
     ];
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
